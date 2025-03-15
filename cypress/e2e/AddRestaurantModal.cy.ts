3da70b990d16b0e9055d2f85d8efde86f83b64c0
hwanheejung <hwanheejung117@gmail.com>
2025-03-15 15:53:53 +0900

    refactor: cypress ts 마이그레이션

--- cypress/e2e/AddRestaurantModal.cy.ts
+++ cypress/e2e/AddRestaurantModal.cy.ts
@@ -1,7 +1,6 @@
 describe("음식점 정보 입력하는 모달에 대한 E2E 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
-
     cy.get('[data-testid="open-add-restaurant-modal-button"]').click();
   });
 
@@ -11,7 +10,6 @@ describe("음식점 정보 입력하는 모달에 대한 E2E 테스트", () => {
 
   it("모달의 backdrop을 클릭하여 닫을 수 있다.", () => {
     cy.get('[data-testid="modal-backdrop"]').click(0, 0);
-
     cy.get('[data-testid="modal"]').should("not.exist");
   });
 
@@ -29,10 +27,10 @@ describe("음식점 정보 입력하는 모달에 대한 E2E 테스트", () => {
     cy.get('[data-testid="link"]').type("https://www.google.com");
 
     cy.get('[data-testid="submit-restaurant-form"]').submit();
-
     cy.get('[data-testid="modal"]').should("not.exist");
+
     // 리스트 첫 번째 아이템이 추가한 음식점 정보인지 확인
-    cy.get('[data-testid="restaurant-list"')
+    cy.get('[data-testid="restaurant-list"]')
       .children()
       .first()
       .should("contain.text", "가나다");
@@ -42,8 +40,8 @@ describe("음식점 정보 입력하는 모달에 대한 E2E 테스트", () => {
     cy.get('[data-testid="submit-restaurant-form"]').submit();
 
     // 경고창이 뜨는지 확인
-    cy.get('[data-testid="category"]').then(($input) => {
-      expect($input[0].validationMessage).to.exist;
-    });
+    cy.get('[data-testid="category"]')
+      .should("have.prop", "validationMessage")
+      .and("not.be.empty");
   });
 });
